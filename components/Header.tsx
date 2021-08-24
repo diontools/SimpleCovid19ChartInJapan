380edd9985440a24ed3892cb9b0b981cebf4509e
diontools <diontools@users.noreply.github.com>
2021-08-25 00:04:12 +0900

    fix header design

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,17 +20,21 @@ export const Header: FunctionComponent<Props> = props => {
             </span>
             <span className="pr-4">{props.prefecture?.title || '全国'}</span>
             <span>更新時刻: {moment.utc(props.updatedAt).utcOffset(9 * 60).format('YYYY / MM / DD (ddd) HH:mm')}</span>
-            {isOpen && <div>
-                <div><Link href="/" linkType="Text">全国</Link></div>
-                {regions.map(region => <div key={region.name}>
-                    {region.name}：{region.prefectures.map(pref => <Link key={pref.id} href={'/' + pref.name} linkType="Text" className='m-1'>{pref.title}</Link>)}
-                </div>)}
-
-                <div className="mt-4">
-                    感染状況データソース: <Link href="https://covid19.mhlw.go.jp/extensions/public/index.html" linkType="Text">データからわかる－新型コロナウイルス感染症情報－ | 厚生労働省</Link>
+            {isOpen && <div className="lg:flex">
+                <div className="flex-none">
+                    <div><Link href="/" linkType="Text">全国</Link></div>
+                    {regions.map(region => <div key={region.name}>
+                        {region.name}：{region.prefectures.map(pref => <Link key={pref.id} href={'/' + pref.name} linkType="Text" className='m-1'>{pref.title}</Link>)}
+                    </div>)}
                 </div>
-                <div>
-                    ワクチンデータソース: <Link href="https://cio.go.jp/c19vaccine_dashboard" linkType="Text">新型コロナワクチンの接種状況（一般接種（高齢者含む）） | 政府CIOポータル</Link>
+
+                <div className="flex-1 mt-4 lg:mt-0">
+                    <div>
+                        感染状況データソース: <Link href="https://covid19.mhlw.go.jp/extensions/public/index.html" linkType="Text">データからわかる－新型コロナウイルス感染症情報－ | 厚生労働省</Link>
+                    </div>
+                    <div>
+                        ワクチンデータソース: <Link href="https://cio.go.jp/c19vaccine_dashboard" linkType="Text">新型コロナワクチンの接種状況（一般接種（高齢者含む）） | 政府CIOポータル</Link>
+                    </div>
                 </div>
             </div>}
         </header>
